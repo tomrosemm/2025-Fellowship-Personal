bf0acedd79ecdf8ad831bb8433afd879fbc664fd
Tom Rose <tomrose@umich.edu>
2025-05-27 19:05:45 -0400

    bump

--- OMNET++/Learning OMNeT++/tictoc/txc1.cc
+++ OMNET++/Learning OMNeT++/tictoc/txc1.cc
@@ -5,6 +5,7 @@
  *      Author: trose
  */
 
+#include <stdio.h>
 #include <string.h>
 #include <omnetpp.h>
 
@@ -17,6 +18,9 @@ using namespace omnetpp;
  */
 class Txc1 : public cSimpleModule
 {
+private:
+    int counter;
+
   protected:
     // The following redefined virtual function holds the algorithm.
     virtual void initialize() override;
@@ -32,10 +36,17 @@ void Txc1::initialize()
     // To bootstrap the tic-toc-tic-toc process, one of the modules needs
     // to send the first message. Let this be `tic'.
 
-    // Am I Tic or Toc?
-    if (strcmp("tic", getName()) == 0) {
-        // create and send first message on gate "out". "tictocMsg" is an
-        // arbitrary string which will be the name of the message object.
+    counter = par("limit");
+
+    // The WATCH() statement below will let you examine the variable under
+    // Qtenv. After doing a few steps in the simulation, click either
+    // `tic' or `toc', and you'll find its `counter' variable and its
+    // current value displayed in the inspector panel (bottom left).
+    WATCH(counter);
+
+
+    if (par("sendMsgOnInit").boolValue() == true) {
+        EV << "Sending initial message\n";
         cMessage *msg = new cMessage("tictocMsg");
         send(msg, "out");
     }
@@ -43,10 +54,17 @@ void Txc1::initialize()
 
 void Txc1::handleMessage(cMessage *msg)
 {
-    // The handleMessage() method is called whenever a message arrives
-    // at the module. Here, we just send it to the other module, through
-    // gate `out'. Because both `tic' and `toc' does the same, the message
-    // will bounce between the two.
-    send(msg, "out"); // send out the message
+    counter --;
+    if (counter == 0) {
+    // If counter is zero, delete message. If you run the model, you'll
+    // find that the simulation will stop at this point with the message
+    // "no more events".
+    EV << getName() << "'s counter reached zero, deleting message\n";
+    delete msg;
+    }
+    else {
+        EV << getName() << "'s counter is " << counter << ", sending back message\n";
+        send(msg, "out");
+    }
 }
 
