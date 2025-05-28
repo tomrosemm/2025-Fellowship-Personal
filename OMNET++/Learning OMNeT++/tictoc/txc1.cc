b61e3871cf07da90aad812484dcfd7197f093765
Tom Rose <tomrose@umich.edu>
2025-05-27 20:30:55 -0400

    bump

--- OMNET++/Learning OMNeT++/tictoc/txc1.cc
+++ OMNET++/Learning OMNeT++/tictoc/txc1.cc
@@ -1,9 +1,12 @@
-/*
- * txc1.cc
- *
- *  Created on: May 27, 2025
- *      Author: trose
- */
+//
+// This file is part of an OMNeT++/OMNEST simulation example.
+//
+// Copyright (C) 2003 Ahmet Sekercioglu
+// Copyright (C) 2003-2015 Andras Varga
+//
+// This file is distributed WITHOUT ANY WARRANTY. See the file
+// `license' for details on this and other legal matters.
+//
 
 #include <stdio.h>
 #include <string.h>
@@ -18,22 +21,62 @@ using namespace omnetpp;
  */
 class Txc1 : public cSimpleModule
 {
-private:
-    // Set the pointers to nullptr, so that the destructor won't crash
-    // even if initialize() doesn't get called because of a runtime
-    // error or user cancellation during the startup process.
-    cMessage *event = nullptr;  // pointer to the event object which we'll use for timing
-    cMessage *tictocMsg = nullptr;  // variable to remember the message until we send it back
-    int counter;
-
   protected:
     // The following redefined virtual function holds the algorithm.
     virtual void initialize() override;
     virtual void handleMessage(cMessage *msg) override;
 };
 
+class Txc2 : public cSimpleModule
+{
+  protected:
+    virtual void initialize() override;
+    virtual void handleMessage(cMessage *msg) override;
+};
+
+/**
+ * In this class we add a counter, and delete the message after ten exchanges.
+ */
+class Txc3 : public cSimpleModule
+{
+  private:
+    int counter;  // Note the counter here
+
+  protected:
+    virtual void initialize() override;
+    virtual void handleMessage(cMessage *msg) override;
+};
+
+/**
+ * In this step you'll learn how to add input parameters to the simulation:
+ * we'll turn the "magic number" 10 into a parameter.
+ */
+class Txc4 : public cSimpleModule
+{
+  private:
+    int counter;
+
+  protected:
+    virtual void initialize() override;
+    virtual void handleMessage(cMessage *msg) override;
+};
+
+class Txc5 : public cSimpleModule
+{
+  private:
+    int counter;
+
+  protected:
+    virtual void initialize() override;
+    virtual void handleMessage(cMessage *msg) override;
+};
+
 // The module class needs to be registered with OMNeT++
 Define_Module(Txc1);
+Define_Module(Txc2);
+Define_Module(Txc3);
+Define_Module(Txc4);
+Define_Module(Txc5);
 
 void Txc1::initialize()
 {
@@ -41,7 +84,30 @@ void Txc1::initialize()
     // To bootstrap the tic-toc-tic-toc process, one of the modules needs
     // to send the first message. Let this be `tic'.
 
-    counter = par("limit");
+    // Am I Tic or Toc?
+    if (strcmp("tic", getName()) == 0) {
+        // create and send first message on gate "out". "tictocMsg" is an
+        // arbitrary string which will be the name of the message object.
+        cMessage *msg = new cMessage("tictocMsg");
+        send(msg, "out");
+    }
+}
+
+void Txc2::initialize()
+{
+    if (strcmp("tic", getName()) == 0) {
+        // The `ev' object works like `cout' in C++.
+        EV << "Sending initial message\n";
+        cMessage *msg = new cMessage("tictocMsg");
+        send(msg, "out");
+    }
+}
+
+void Txc3::initialize()
+{
+    // Initialize counter to ten. We'll decrement it every time and delete
+    // the message when it reaches zero.
+    counter = 10;
 
     // The WATCH() statement below will let you examine the variable under
     // Qtenv. After doing a few steps in the simulation, click either
@@ -49,7 +115,36 @@ void Txc1::initialize()
     // current value displayed in the inspector panel (bottom left).
     WATCH(counter);
 
+    if (strcmp("tic", getName()) == 0) {
+        EV << "Sending initial message\n";
+        cMessage *msg = new cMessage("tictocMsg");
+        send(msg, "out");
+    }
+}
 
+void Txc4::initialize()
+{
+    // Initialize the counter with the "limit" module parameter, declared
+    // in the NED file (tictoc4.ned).
+    counter = par("limit");
+
+    // we no longer depend on the name of the module to decide
+    // whether to send an initial message
+    if (par("sendMsgOnInit").boolValue() == true) {
+        EV << "Sending initial message\n";
+        cMessage *msg = new cMessage("tictocMsg");
+        send(msg, "out");
+    }
+}
+
+void Txc5::initialize()
+{
+    // Initialize the counter with the "limit" module parameter, declared
+    // in the NED file (tictoc4.ned).
+    counter = par("limit");
+
+    // we no longer depend on the name of the module to decide
+    // whether to send an initial message
     if (par("sendMsgOnInit").boolValue() == true) {
         EV << "Sending initial message\n";
         cMessage *msg = new cMessage("tictocMsg");
@@ -59,13 +154,31 @@ void Txc1::initialize()
 
 void Txc1::handleMessage(cMessage *msg)
 {
-    counter --;
+    // The handleMessage() method is called whenever a message arrives
+    // at the module. Here, we just send it to the other module, through
+    // gate `out'. Because both `tic' and `toc' does the same, the message
+    // will bounce between the two.
+    send(msg, "out"); // send out the message
+}
+
+
+void Txc2::handleMessage(cMessage *msg)
+{
+    // msg->getName() is name of the msg object, here it will be "tictocMsg".
+    EV << "Received message `" << msg->getName() << "', sending it out again\n";
+    send(msg, "out");
+}
+
+void Txc3::handleMessage(cMessage *msg)
+{
+    // Increment counter and check value.
+    counter--;
     if (counter == 0) {
-    // If counter is zero, delete message. If you run the model, you'll
-    // find that the simulation will stop at this point with the message
-    // "no more events".
-    EV << getName() << "'s counter reached zero, deleting message\n";
-    delete msg;
+        // If counter is zero, delete message. If you run the model, you'll
+        // find that the simulation will stop at this point with the message
+        // "no more events".
+        EV << getName() << "'s counter reached zero, deleting message\n";
+        delete msg;
     }
     else {
         EV << getName() << "'s counter is " << counter << ", sending back message\n";
@@ -73,3 +186,28 @@ void Txc1::handleMessage(cMessage *msg)
     }
 }
 
+void Txc4::handleMessage(cMessage *msg)
+{
+    counter--;
+    if (counter == 0) {
+        EV << getName() << "'s counter reached zero, deleting message\n";
+        delete msg;
+    }
+    else {
+        EV << getName() << "'s counter is " << counter << ", sending back message\n";
+        send(msg, "out");
+    }
+}
+
+void Txc5::handleMessage(cMessage *msg)
+{
+    counter--;
+    if (counter == 0) {
+        EV << getName() << "'s counter reached zero, deleting message\n";
+        delete msg;
+    }
+    else {
+        EV << getName() << "'s counter is " << counter << ", sending back message\n";
+        send(msg, "out");
+    }
+}
