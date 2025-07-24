61200bad4f9669999470f8ae92c10ade891d6ebb
Tom Rose <tomrose@umich.edu>
2025-07-24 13:53:25 -0400

    s

--- Python/Basic Concept/preliminary_tests.py
+++ Python/Basic Concept/preliminary_tests.py
@@ -752,12 +752,12 @@ def testAndScenarioRunner():
     # clear_console()
 
     # 14 - Run SUMO TraCI Data Transfer Test
-    test_sumo_traci_data_transfer(False)
+    test_sumo_traci_data_transfer(True)
     time.sleep(.5)
     # clear_console()
 
     # 15 - Run SUMO TraCI Data Transfer Test (.sumocfg, 100 steps)
-    test_sumo_traci_data_transfer_sumocfg(False)
+    test_sumo_traci_data_transfer_sumocfg(True)
     time.sleep(.5)
     # clear_console()
 
